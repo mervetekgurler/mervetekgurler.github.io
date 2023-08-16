2dec809865620fe14e5b157ae8306d26b8431b61
Merve Tekgürler <43646055+mervetekgurler@users.noreply.github.com>
2023-08-16 01:11:14 -0700

    attempt2

--- ottomantranslator/script.js
+++ ottomantranslator/script.js
@@ -1,15 +1,71 @@
-async function translateText() {
-    const inputText = document.getElementById("inputText").value;
-    const response = await fetch("YOUR_SERVERLESS_FUNCTION_URL_HERE", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            text: inputText
-        })
-    });
-    
-    const data = await response.json();
-    document.getElementById("outputText").value = data.translation;
+async function translate() {
+    const ottomanInput = document.getElementById("ottomanInput").value;
+
+    if (ottomanInput.length === 0) {
+        alert("Please input an Ottoman Turkish sentence.");
+        return;
+    }
+
+    if (ottomanInput.split(' ').length > 100) {
+        alert("Please limit the input to 100 words.");
+        return;
+    }
+
+    try {
+        const response = await fetch("https://your-vercel-deployment-url/translate", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ sentence: ottomanInput })
+        });
+
+        if (response.status !== 200) {
+            throw new Error('Failed to get translation');
+        }
+
+        const data = await response.json();
+        document.getElementById("englishOutput").value = data.translation;
+
+    } catch (err) {
+        console.error(err);
+        alert("Error getting translation.");
+    }
 }
+
+async function submitFeedback() {
+    const feedback = document.getElementById("comments").value;
+    const ottomanInput = document.getElementById("ottomanInput").value;
+    const englishOutput = document.getElementById("englishOutput").value;
+
+    if (!feedback) {
+        alert("Please provide your feedback before submitting.");
+        return;
+    }
+
+    try {
+        const response = await fetch("https://your-vercel-deployment-url/feedback", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ 
+                feedback,
+                ottomanInput,
+                englishOutput
+            })
+        });
+
+        if (response.status !== 200) {
+            throw new Error('Failed to submit feedback');
+        }
+
+        alert("Thank you for your feedback!");
+        document.getElementById("comments").value = "";
+
+    } catch (err) {
+        console.error(err);
+        alert("Error submitting feedback.");
+    }
+}
+
